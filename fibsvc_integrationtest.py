56443facbca4ff63d8ba2d34ef7b50d2a926c707
Rene Hernandez <renehdz@gmail.com>
2013-12-13 15:33:59 -0700

    Update fibsvc_integrationtest.py
    Use values from httplib. Fixed put and delete tests to actually send put and delete requests.

--- fibsvc_integrationtest.py
+++ fibsvc_integrationtest.py
@@ -8,6 +8,7 @@ import json
 import unittest
 import random
 from fibsvc import MIN_N, MAX_N
+from httplib import OK, BAD_REQUEST, NOT_FOUND, METHOD_NOT_ALLOWED
 from webtest import TestApp
 
 class FibonacciServiceTestCase(unittest.TestCase):
@@ -25,7 +26,7 @@ class FibonacciServiceTestCase(unittest.TestCase):
         if MIN_N == 0:
             res = self.app.get("/fibsvc/0")
             self.assertEqual("application/json", res.content_type)
-            self.assertEqual(200, res.status_int)
+            self.assertEqual(OK, res.status_int)
             self.assertTrue("fibonacci" in res.json.keys())
 
             seq = res.json.values()
@@ -36,7 +37,7 @@ class FibonacciServiceTestCase(unittest.TestCase):
     def test_fibsvc_negative(self):
         res = self.app.get("/fibsvc/-10", expect_errors = True)
         self.assertEqual("application/json", res.content_type)
-        self.assertEqual(400, res.status_int)
+        self.assertEqual(BAD_REQUEST, res.status_int)
 
         keys = res.json.keys()
         self.assertTrue("error_type" in keys)
@@ -47,7 +48,7 @@ class FibonacciServiceTestCase(unittest.TestCase):
     def test_fibsvc_smaller_than_min(self):
         res = self.app.get("/fibsvc/%d" % (MIN_N - 1), expect_errors = True)
         self.assertEqual("application/json", res.content_type)
-        self.assertEqual(400, res.status_int)
+        self.assertEqual(BAD_REQUEST, res.status_int)
 
         keys = res.json.keys()
         self.assertTrue("error_type" in keys)
@@ -58,7 +59,7 @@ class FibonacciServiceTestCase(unittest.TestCase):
     def test_fibsvc_larger_than_max(self):
         res = self.app.get("/fibsvc/%d" % (MAX_N + 1), expect_errors = True)
         self.assertEqual("application/json", res.content_type)
-        self.assertEqual(400, res.status_int)
+        self.assertEqual(BAD_REQUEST, res.status_int)
 
         keys = res.json.keys()
         self.assertTrue("error_type" in keys)
@@ -68,39 +69,39 @@ class FibonacciServiceTestCase(unittest.TestCase):
         
     def test_fibsvc_no_param(self):
         res = self.app.get("/fibsvc", expect_errors = True)
-        self.assertEqual(404, res.status_int)
+        self.assertEqual(NOT_FOUND, res.status_int)
         
         
     def test_fibsvc_string(self):
         res = self.app.get("/fibsvc/hi", expect_errors = True)
-        self.assertEqual(404, res.status_int)
+        self.assertEqual(NOT_FOUND, res.status_int)
         
         
     def test_fibsvc_float(self):
         res = self.app.get("/fibsvc/2.5", expect_errors = True)
-        self.assertEqual(404, res.status_int)
+        self.assertEqual(NOT_FOUND, res.status_int)
         
         
     def test_fibsvc_post(self):
         res = self.app.post("/fibsvc/10", expect_errors = True)
-        self.assertEqual(405, res.status_int)
+        self.assertEqual(METHOD_NOT_ALLOWED, res.status_int)
         
         
     def test_fibsvc_put(self):
-        res = self.app.get("/fibsvc/hi", expect_errors = True)
-        self.assertEqual(404, res.status_int)
+        res = self.app.put("/fibsvc/hi", expect_errors = True)
+        self.assertEqual(NOT_FOUND, res.status_int)
         
         
     def test_fibsvc_delete(self):
-        res = self.app.get("/fibsvc/hi", expect_errors = True)
-        self.assertEqual(404, res.status_int)
+        res = self.app.delete("/fibsvc/hi", expect_errors = True)
+        self.assertEqual(NOT_FOUND, res.status_int)
         
         
     def test_fibsvc_valid(self):
         n = random.randint(MIN_N, MAX_N)
         res = self.app.get("/fibsvc/%d" % n)
         self.assertEqual("application/json", res.content_type)
-        self.assertEqual(200, res.status_int)
+        self.assertEqual(OK, res.status_int)
         self.assertTrue("fibonacci" in res.json.keys())
 
         seq = res.json.values()[0]
